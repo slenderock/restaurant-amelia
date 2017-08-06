fd294b8cfb5108e50d2742366122435216311a6f
Flippylolz <assasin463@gmail.com>
2017-08-06 12:56:50 +0300

    delete test folder

--- test/controllers/reserve_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ReserveControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get reserve_show_url
-    assert_response :success
-  end
-
-end
